fa51d9c6b2458510536f25d09a4c4e0c1e0cce5d
Murat Polat <mpolatpy@gmail.com>
2022-01-18 21:48:19 -0500

    updated settings

--- hcss_app/settings.py
+++ hcss_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0p3k66!r=+zj&m&d@m#j*)fjh2n08$&t7)#&m8%ffl+6c*cpv^'
+SECRET_KEY = '###update_this_later#####'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
